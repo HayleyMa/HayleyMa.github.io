6b50891fd7334b28d50c5bd9213e1e466dda1500
Hayley <hayleyma16@gmail.com>
2019-05-03 15:21:55 -0500

    Finished Map

--- finalMap/map.js
+++ finalMap/map.js
@@ -1,59 +1,44 @@
 function init () {
   // Begin map 1
-  let map1 = L.map('map1').setView([32.18, -99.14], 4)
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    // attribution: 'Thunderforest'
-  }).addTo(map1)
-  // let worthUrl = "worth.geojson"
-  // // L.geoJSON('worth.geojson').addTo(map);
-  // jQuery.getJSON(worthUrl, function (data) {
-  // L.geoJSON(data).addTo(map1)
-  // })
-  //L.geoJSON(data, { style: { color: 'green' } }).addTo(map1)
-  function popUp(feature, layer) {
-    layer.bindPopup(feature.properties.Restaurant_Name);
+  let map1 = L.map('map1').setView([34.0522, -118.2437], 10)
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map1)
+  let worthUrl = 'https://hayleyma.github.io/finalMap/worth.geojson'
+  let popUp = function (feature, layer) {
+    layer.bindPopup(feature.properties.Restaurant_Name+'<br>'
+                    + feature.properties.Food_Type+'<br>'
+                    + feature.properties.Price);
   }
-  var geojsonLayer = new L.GeoJSON.AJAX("worth_0.geojson", {onEachFeature:popUp});
-
-  //var geojsonLayer = new L.GeoJSON.AJAX("./worth.geojson");
-  geojsonLayer.addTo(map1);
-
-  // let stateStyle = function (data) {
-  //   let age = feature.properties.MED_AGE // get the current state's Median Age attribute
-  //   let stateColor = 'olive' // let the initial color be a darker green
-  //   if ( age < 38 ) { stateColor = 'green' } // if the state's median age is less than the average, color it a lighter green
-  //   return {
-  //     color: stateColor, //use the color variable above for the value
-  //     weight: 1,
-  //     fillOpacity: 0.2
-  //   }
-  // }
-
-
-
-  let city = L.marker([30, -90]).addTo(map1)
-  let area = L.polygon([
-    [31, -88],
-    [35, -88],
-    [32, -82]
-  ]).addTo(map1)
-  area.bindPopup('A polygon')
-  city.bindPopup('A marker')
-
+  let options = {
+    onEachFeature: popUp
+   }
+   jQuery.getJSON(worthUrl, function (data) {
+     L.geoJSON(data, options).addTo(map1)
+   })
   // Begin map 2
-  let map2 = L.map('map2').setView([37, -95], 4)
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map2)
-  L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
-    layers: 'nexrad-n0r-900913',
-    format: 'image/png',
-    transparent: true,
-    attribution: 'NOAA, Iowa State University'
-  }).addTo(map2)
-  L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
-    layers: 'warnings_c',
-    format: 'image/png',
-    transparent: true,
-    attribution: 'NOAA, Iowa State University'
-  }).addTo(map2)
+  let map2 = L.map('map2').setView([37.566, 126.9784], 7)
+  L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png').addTo(map2)
+  jQuery.getJSON(worthUrl, function (data) {
+    L.geoJSON(data, options).addTo(map2)
+  })
+  //Map 3
+  let map3 = L.map('map3').setView([40.76581, -73.97939], 11)
+  L.tileLayer('https://{s}.tile.thunderforest.com/transport-dark/{z}/{x}/{y}.png').addTo(map3)
+  jQuery.getJSON(worthUrl, function (data) {
+    L.geoJSON(data, options).addTo(map3)
+  })
+//Map 4
+let map4 = L.map('map4').setView([36.113708, -115.172768], 10)
+L.tileLayer('https://map1.vis.earthdata.nasa.gov/wmts-webmerc/VIIRS_CityLights_2012/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}', {
+	attribution: 'Imagery provided by services from the Global Imagery Browse Services (GIBS), operated by the NASA/GSFC/Earth Science Data and Information System (<a href="https://earthdata.nasa.gov">ESDIS</a>) with funding provided by NASA/HQ.',
+	bounds: [[-85.0511287776, -179.999999975], [85.0511287776, 179.999999975]],
+	minZoom: 1,
+	maxZoom: 8,
+	format: 'jpg',
+	time: '',
+	tilematrixset: 'GoogleMapsCompatible_Level'
+}).addTo(map4);
+jQuery.getJSON(worthUrl, function (data) {
+  L.geoJSON(data, options).addTo(map4)
+})
 }
 window.addEventListener('load', init)
